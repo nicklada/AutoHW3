1301672d6c5ab914bed4cd3fb4c98e06e412d4c2
lada <nicklada94@gmail.com>
2020-06-04 14:29:27 +0300

    Yml updated

--- src/test/java/FormTest.java
+++ src/test/java/FormTest.java
@@ -2,6 +2,7 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
@@ -10,6 +11,7 @@ import static com.codeborne.selenide.Selenide.*;
 import static org.openqa.selenium.By.cssSelector;
 
 public class FormTest {
+
     @AfterEach
     void tearDown(){
         closeWindow();
